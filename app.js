2d83cce0876c21cd898924986c0f1fba379de877
Abigail Fernandes <abigailferns94@gmail.com>
2019-03-15 18:51:18 -0600

    Feature - Enable CORS

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import trendsController from './controllers/trendsController';
+import cors from 'cors';
+
 const app = express();
+app.use(cors)
 
 app.use('/bigmoodapi', trendsController);
 
