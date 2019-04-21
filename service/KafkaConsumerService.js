08fcdc13b3b6e6176ed14857e42102e68819c12f
srishtirawal <srishti.rawal2012@vit.ac.in>
2019-04-20 21:08:31 -0600

    Added kafka consumer

---
+++ service/KafkaConsumerService.js
@@ -0,0 +1,33 @@
+import { insertAllTrendsLocationWise } from './RedisCacheService';
+import kafka from 'kafka-node';
+import { builtinModules } from 'module';
+import { logger } from './LoggerService';
+import util from 'util';
+// var kafka = require('kafka-node')
+let options = {
+  fromOffset: 'latest'
+};
+
+const consumerClient = new kafka.KafkaClient('localhost:2181');
+
+const consumer = new kafka.Consumer(
+  consumerClient,
+  [{ topic: 'trendSentiment', partition: 0, fromOffset: -1 }],
+  [
+    {
+      autoCommit: false
+    },
+    (options = {
+      fromOffset: 'latest'
+    })
+  ]
+);
+
+const consumeTrendsFromKafka = () => {
+  consumer.on('message', message => {
+    const trends = JSON.parse(message.value);
+    logger.info('Received trends from Kafka topic');
+    insertAllTrendsLocationWise(trends);
+  });
+};
+module.exports = { consumeTrendsFromKafka };
