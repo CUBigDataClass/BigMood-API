f6f582ba70d77cbcbe466de9d03a9d042ea8b82b
Abigail Fernandes <abigailferns94@gmail.com>
2019-04-20 19:18:54 -0600

    Feature -  Functions for querying and inserting into Redis

--- service/RedisCacheService.js
+++ service/RedisCacheService.js
@@ -23,8 +23,7 @@ const getTrendingTopicsFromRedis = cacheKey => {
       });
     } catch (error) {
       logger.error(
-        'RedisCacheService: getTrendingTopicsFromRedis: Error occured: ' +
-          error
+        'RedisCacheService: getTrendingTopicsFromRedis: Error occured: ' + error
       );
     }
   });
@@ -49,6 +48,71 @@ const cacheTrendsInRedis = (cacheKey, topic) => {
     } catch (error) {
       logger.error('Error occured: ' + error);
     }
+  });
+};
+
+// Create key for a object to be inserted based on locationType
+const createKeyForLocation = value => {
+  if (value.locationType == 'Country') {
+    return 'trend' + value.locationType + '-' + value.country;
+  } else if (value.locationType == 'City') {
+    return (
+      'trend' + value.locationType + '-' + value.country + '-' + value.country
+    );
+  }
+};
+
+// 
+const insertTrendLocationWise = value => {
+  return new Promise((resolve, reject) => {
+    key = createKeyForLocation(value);
+    RedisClient.client.set(key, value, (err, res) => {
+      if (err) {
+        logger.error('Failed to update the redis cache: ' + err);
+        reject(err);
+      }
+      resolve(res);
+    });
+  });
+};
+
+const insertAllTrendsLocationWise = trends => {
+  trends.forEach(element => {
+    insertTrendLocationWise(element);
+  });
+};
+
+// Find all keys matching a given pattern
+const getAllKeys = pattern => {
+  RedisClient.client.keys(pattern, (err, res) => {
+    if (err) {
+      logger.error('Failed to get keys matching pattern: ' + pattern);
+      reject(err);
+    }
+    resolve(res);
+  });
+};
+
+// Gets alls trends by finding all the keys in redis that start with 'trend'
+const getAllTrends = () => {
+  return new Promise((resolve, reject) => {
+    const keys = getAllKeys('trend*');
+    if (!keys) {
+      logger.error('No keys returned by Redis with trend in key')
+      reject(new Error('No keys returned by Redis with trend in key'))
+    }
+    const result = [];
+    keys.forEach(key => {
+      RedisClient.client.get(key, (err, res) => {
+        if (err) {
+          logger.error('Error in getting value for key:' + key);
+        } else {
+          logger.info(res);
+          result.push(res);
+        }
+      });
+      resolve(result);
+    });
   });
 };
 
