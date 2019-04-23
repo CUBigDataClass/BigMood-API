8ed79ed442fe17e866e9a79ac603ca2ed2612621
Abigail Fernandes <abigailferns94@gmail.com>
2019-04-22 23:42:03 -0600

    Revert "Feature: Add url to tweet data and increase trends to 3"
    This reverts commit 8fa7f5400b713654a2eaf6e29b7c033840859e5e.

--- cronGetTrends.js
+++ cronGetTrends.js
@@ -94,7 +94,7 @@ const getTrendsByCountry = countryWoeids => {
     countryWoeids.map(item =>
       twitterClient
         .get('trends/place.json', { id: item.woeid })
-        .then(result => getTrendingHashTag(result[0].trends, 3, item))
+        .then(result => getTrendingHashTag(result[0].trends, 1, item))
         .catch(error => logger.error(error))
     )
   ).then(data => {
@@ -125,8 +125,7 @@ const getTrendingHashTag = (trends, numOfTrends, countryWoeid) => {
     const newObj = {
       name: item.name,
       tweetVolume: item.tweet_volume,
-      rank: null,
-      url: item.url
+      rank: null
     };
     return newObj;
   });
